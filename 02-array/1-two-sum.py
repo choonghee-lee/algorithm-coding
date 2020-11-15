a472c380da732ab116b08f6e11c225f44091a8fa
choonghee-lee <choonghee.dev@gmail.com>
2020-11-15 12:55:31 +0900

    [+] 1. Two Sum

---
+++ 02-array/1-two-sum.py
@@ -0,0 +1,11 @@
+"""
+1. Two Sum
+    https://leetcode.com/problems/two-sum/
+"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, n in enumerate(nums):
+            if target - n in d:
+                return i, d[target-n]
+            d[n] = i
